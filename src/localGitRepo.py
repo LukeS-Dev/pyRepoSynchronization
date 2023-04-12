094ed5bb983266ae1bae1dfbb7a64876634fe52b
Luke <110707048+LukeS-Dev@users.noreply.github.com>
2023-04-12 10:17:21 +1000

    Modified code to accept Git overwrite.
    For machines without "git" in the system path or if a different version of git needs to be used. GIT_PYTHON_GIT_EXECUTABLE environment variable can be defined in the .env

--- src/localGitRepo.py
+++ src/localGitRepo.py
@@ -8,10 +8,16 @@ load_dotenv()
 def getLocalRepoHash():
     
     LOCAL_GIT_REPO_PATH = os.getenv("LOCAL_GIT_REPO_PATH")
+    GIT_PYTHON_GIT_EXECUTABLE = os.getenv("GIT_PYTHON_GIT_EXECUTABLE")
+
+    if(not GIT_PYTHON_GIT_EXECUTABLE or GIT_PYTHON_GIT_EXECUTABLE == ''):
+        git_exe_cmd = 'git'
+    else: 
+        git_exe_cmd = GIT_PYTHON_GIT_EXECUTABLE
 
     # Change the current working directory to the repository path
     # so that the git command is executed in the context of the repo
-    with subprocess.Popen(['git', 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, cwd=LOCAL_GIT_REPO_PATH) as process:
+    with subprocess.Popen([git_exe_cmd, 'rev-parse', 'HEAD'], stdout=subprocess.PIPE, cwd=LOCAL_GIT_REPO_PATH) as process:
         # Get the output of the git command
         output = process.communicate()[0].strip()
 
